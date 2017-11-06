6788a9a2415f8b64a25851dbaf17adf5c781e93a
humorfish <m18744017952@163.com>
2017-11-07 00:47:24 +0800

    login formgroup

--- src/authorize/login/login.comp.ts
+++ src/authorize/login/login.comp.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({selector: 'c-login', templateUrl: 'login.comp.html', styleUrls: ['login.comp.scss']})
 export class LoginComp implements OnInit
@@ -9,5 +10,12 @@ export class LoginComp implements OnInit
 
     ngOnInit()
     {
+        this.LoginForm = new FormGroup(
+            {
+                UserName: new FormControl()
+            }
+        );
     }
+
+    LoginForm: FormGroup;
 }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,14 +1,19 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         TranslateModule
     ],
     exports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         TranslateModule
     ]
 
