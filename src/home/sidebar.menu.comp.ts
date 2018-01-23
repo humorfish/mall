b317f3f37b72e7e5a661efb1196570d849c1f843
humorfish <m18744017952@163.com>
2018-01-24 00:02:40 +0800

    sidebar

--- src/home/sidebar.menu.comp.ts
+++ src/home/sidebar.menu.comp.ts
@@ -10,17 +10,56 @@ export class SidebarMenuComp
 {
     constructor()
     {
-
     }
 
-    inputSearchText(v: any)
+    inputSearchText(event: any)
     {
-
+        this.searchText = event.target.value;
     }
 
-    searchMenu()
+    checkSearchMenuIdExists(id: string, menuIdList: Array<string>)
     {
+        for (let menuId of menuIdList)
+        {
+           if (menuId === id)
+           {
+              return true;
+           }
+        }
 
+        return false;
+    }
+
+    searchItem(item: MenuData, menuList: Array<MenuData>, menuIdList: Array<string>, keyWord: RegExp)
+    {
+        item.isExpend = false;
+
+        if ((item.name.match(keyWord) || item.keyWord.match(keyWord)) &&
+            ! this.checkSearchMenuIdExists(item.id, menuIdList))
+        {
+                menuList.push(item);
+                this.pushSearchMenu(item, menuIdList);
+        }
+
+        let itemChildren = item.children;
+        if (itemChildren && itemChildren.length > 0)
+        {
+            for (let subItem of itemChildren)
+            {
+                this.searchItem(subItem, menuList, menuIdList, keyWord);
+            }
+        }
+    }
+
+    pushSearchMenu(item: MenuData, menuIdList: Array<string>)
+    {
+        menuIdList.push(item.id);
+        let itemChildren = item.children;
+        if (itemChildren && itemChildren.length > 0)
+        {
+            for (let subItem of itemChildren)
+                this.pushSearchMenu(subItem, menuIdList);
+        }
     }
 
     itemClicked(item: MenuData)
@@ -35,10 +74,11 @@ export class SidebarMenuComp
 
     isLeaf(item: MenuData)
     {
-
+        return (! item.children || ! item.children.length);
     }
 
 
     @Input() data: Array<MenuData>;
     searchMsgHidden: boolean = false;
+    searchText: string = '';
 }
